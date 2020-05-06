da22c736879f0a07af0249f1cd1a83c4114f9380
Sibusiso <itsbudamain.05@gmail.com>
2020-05-06 13:15:00 +0200

    created basic files

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 A python todolist
 
 pre-requisites
--Python
--OOP
--tkinter
+- Python
+- OOP
+- tkinter
+
+##Todo
+- Design dashboard
+- Design Menu
+- Design TodoFrame
+- Design Todos
+
--- micro.py
+++ micro.py
@@ -1,1 +1,27 @@
-"hello world" 
+import tkinter as tk
+from tkinter import ttk
+
+class Application(tk.Frame):
+	def __init__(self, master = None):
+		super().__init__(master)
+		self.master = master
+		self.pack()
+		self.createDashboard()
+
+	def createDashboard(self):
+		pass
+		# self.menu = createMenu()
+		# menu.pack(side='left')
+
+		# self.todoFrame = createTodoFrame()
+		# menu.pack(side='right') 
+ 
+
+def main():
+	root = tk.Tk()
+
+	app = Application(master=root)
+	app.mainloop()
+
+if __name__ == '__main__':
+	main()
