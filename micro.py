a81434f936ee032df4a7f2fbab9794e285ceb98b
Sibusiso <itsbudamain.05@gmail.com>
2020-05-15 22:59:25 +0200

    added basic widgets

--- micro.py
+++ micro.py
@@ -2,6 +2,11 @@ import tkinter as tk
 from tkinter import font
 from tkinter import ttk
 
+# color_scheme
+light_red = '#ff2211'
+
+my_todos = ['Check','me','out'] 
+
 class App():
 	def __init__(self):
 		self.window = tk.Tk()
@@ -9,46 +14,52 @@ class App():
 		self.window.state('zoomed')
 		self.h, self.w = self.window.winfo_screenheight(), self.window.winfo_screenwidth()
 
-		style = ttk.Style()
-		style.configure('Menu.TFrame', background='Blue', width=(self.w*0.3))
-
 		self.frm_menu = self.create_menu()
 
-		style.configure('Todo.TFrame,', background='White', width=(self.w*0.7))
 		self.frm_todo = self.create_todo_frame()
 
-		self.frm_menu.pack(side='left', expand=False, fill='both')
-		self.frm_todo.pack(side='right', expand=True, fill='both')
+		self.frm_menu.place(relx=0, rely=0, relwidth= 0.3, relheight=1)
+		self.frm_todo.place(relx=0.3, rely=0, relwidth= 0.7, relheight=1)
 
 	def run(self):
 		self.window.mainloop()
 
 	def create_menu(self):
-		frm_menu = ttk.Frame(self.window, style='Menu.TFrame', padding=(100,100))
+		frm_menu = tk.Frame(self.window, bg=light_red)
 
 		style = ttk.Style()
-		style.configure('WB.TLabel', foreground='White', background='Blue', font=('Helvitica', 25, 'underline bold'))
+		style.configure('WB.TLabel', foreground='White', background=light_red, font=('Helvitica', 25, 'underline bold'))
 
 		lbl_head = ttk.Label(frm_menu, text='Projects', style='WB.TLabel')
-		lbl_head.pack()
+		lbl_head.place(rely=0.1, anchor='w')
 		
 		return frm_menu
 
 	def create_todo_frame(self):
-		frm_todo = ttk.Frame(self.window, style='Todo.TFrame', padding=60)
+		frm_todo = tk.Frame(self.window)
 
-		style = ttk.Style()
-		style.configure('H1.TLabel', foreground='Black', font=('Helvitica', 30))
+		lbl_head = ttk.Label(frm_todo, text='Project')
+		lbl_head.pack()
 
-		lbl_Head = ttk.Label(frm_todo, text='Todo List', style='H1.TLabel')
-		lbl_Head.pack(side='top')
+		my_entry = ttk.Entry(frm_todo)
+		my_entry.pack()
 
-		style.configure('P.TEntry', font=('Arial', 15), height=20, width = 50)
-		entry_todo = ttk.Entry(frm_todo)
-		entry_todo.pack(side='top')
-		
+		# border radius
+		btn_add = ttk.Button(frm_todo, text ='+')
+		btn_add.pack()
+
+		self.show_todos(frm_todo)
+
 		return frm_todo
 
+	def show_todos(self, frame):
+		for task in my_todos:
+			lbl_task = tk.Label(frame, text=task)
+			lbl_task.pack()
+
+			# border radius
+			btn_del = ttk.Button(frame, text='Remove')
+			btn_del.pack()
 
 def main():
 	app = App()
